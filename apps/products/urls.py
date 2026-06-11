bb01ad46b3faf3b7879a3e09422b4b19e74afb7a
Константин С. Васильев <K118@sa.local>
2026-06-11 09:22:21 +0700

    ispravil adminlist

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', views.admin_product_list, name='admin_product_lsit'),
+    path('admin/', views.admin_product_list, name='admin_product_list'),
     path('admin/create/', views.admin_product_create, name='admin_product_create'),
     path('admin/<int:pk>/edit/', views.admin_product_update, name='admin_product_update'),
     path('admin/<int:pk>/delete/', views.admin_product_delete, name='admin_product_delete'),
