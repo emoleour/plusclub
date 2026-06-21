3a0b8e4ce850bd6d3bfb5df10a2ffcd7ee41c399
Konstantin Vasilev <emoleour.mu@gmail.com>
2026-06-21 16:58:40 +0700

    update reset password and design

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -16,4 +17,22 @@ urlpatterns = [
     path('admin/admins/create', views.admin_create, name='admin_create'),
     path('admin/admins/<int:pk>/edit', views.admin_update, name='admin_update'),
     path('admin/admins/<int:pk>/delete', views.admin_delete, name='admin_delete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html',
+            html_email_template_name='users/password_reset_email.html',
+            subject_template_name='users/password_reset_subject.txt'
+            ),
+        name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'
+    ),
+    name='password_reset_complete'),
 ]
